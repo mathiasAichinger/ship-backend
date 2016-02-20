1696c66ba55f72cec2dca723eb808c8697e061d5
Mathias Aichinger <mathias.aichinger@runtastic.com>
2016-02-20 14:18:04 +0100

    MOD updated docu

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,18 +1,18 @@
 class AppsController < ApplicationController
   resource_description do
     formats [:json]
-    short "API for managing jobs"
+    short "API for managing apps"
   end
 
-  api :GET, '/jobs', "Returns all jobs"
+  api :GET, '/apps', "Returns all apps"
   param :included, String, "Name of the related entities which should be included beside the base resource"
   def index
     included = params[:include].try(:split, ",")
     render json: App.all, include: included
   end
 
-  api :GET, '/jobs/:id', "Returns the job with the given id"
-  desc "Returns the job with the given id"
+  api :GET, '/apps/:id', "Returns the app with the given id"
+  desc "Returns the app with the given id"
   param :id, :number, "Id of the App"
   param :included, String, "Name of the related entities which should be included beside the base resource"
   def show
